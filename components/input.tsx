68f39866c4e224291590b4ac2d03e60ea9b717e4
Suji Park <psjhero94@naver.com>
2025-07-05 18:47:17 +0900

    Recap - Refactor

--- components/input.tsx
+++ components/input.tsx
@@ -5,10 +5,12 @@ interface InputProps {
 	errors?: string[];
 }
 
+// input 이 받을 수 있는 모든 attributes를 받을 수 있게 한다
+// Input 컴포넌트를 더 확장성 있고 커스텀 가능하게 만든다
 export default function Input({
-	name,
+	name, // 필수 항목이므로 남긴다
 	errors = [],
-	...rest
+	...rest // name과 errors를 제외한 모든 props
 }: InputProps & InputHTMLAttributes<HTMLInputElement>) {
 	return (
 		<div className="flex flex-col gap-2">
